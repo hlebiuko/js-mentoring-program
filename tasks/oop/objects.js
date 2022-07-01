084b99eac73765a87863095cd63147995ca25efa
Hleb Iuko <hiuko.contractor@libertyglobal.com>
2022-07-01 17:44:35 +0200

    module6 hometask - version 2, from correct source

---
+++ tasks/oop/objects.js
@@ -0,0 +1,35 @@
+/**
+ * JS Objects: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
+ * create object person with firstName and lastName properties
+ * implement method getFullName that returns full name
+ */
+ const person = {
+	firstName : "John",
+	lastName : "Doe",
+	getFullName () {
+		return `${this.firstName} ${person.lastName}`;
+	}
+}; //put you object here
+
+/**
+ * create object student that has property grade and method getGrade
+ * set person as prototype of student
+ */
+ const student = {
+	grade : "bachelor",
+	getGrade () {
+	  return this.grade;
+	}
+  };
+  Object.setPrototypeOf(student, person);
+
+/**
+ * create new instance of student using Object.create
+ */
+ const student2 = Object.create(student); //put you object here
+
+module.exports = {
+	person,
+	student,
+	student2
+};
